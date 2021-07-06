46615e5fb55b0ef7a25037adf5ddd4499dbc655f
Johnny Magrippis <j@magrippis.com>
2021-07-06 17:17:52 +0100

    🔍  bring in favicon, app icons, update meta tags

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,35 @@ export function Home() {
   return (
     <div className="flex flex-col justify-between items-center h-screen">
       <Head>
-        <title>An app with Next!</title>
+        <title>Eri Panselina | Journalist - PR expert - Link builder</title>
         <meta
           name="description"
-          content="A companion app to YouTube tutorials for making Next.js apps!"
+          content="Eri is a career journalist who is now using her superpowers to help people communicate their mission and
+          understand the world around them."
         />
+        {/* favicon & other app icons */}
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#22d3ee" />
+        <meta name="msapplication-TileColor" content="#22d3ee" />
+        <meta name="theme-color" content="#22d3ee" />
       </Head>
 
       <main className="w-full">
