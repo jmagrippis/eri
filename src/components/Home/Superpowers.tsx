f122a354b98de6fb0f0eabceaf63d238e0b68203
Johnny Magrippis <j@magrippis.com>
2021-11-20 18:43:18 +0000

    ✨  you can now resume typing onClick

--- src/components/Home/Superpowers.tsx
+++ src/components/Home/Superpowers.tsx
@@ -6,11 +6,14 @@ type Props = {
 }
 
 export const Superpowers = ({ superpowers }: Props) => {
-  const { typedSuperpower, selectedSuperpower, phase } =
+  const { typedSuperpower, selectedSuperpower, phase, resume } =
     useTypedSuperpower(superpowers)
 
   return (
-    <h2 className="flex flex-col lg:block text-center pt-12 text-5xl tracking-tight text-gray-900 md:text-6xl">
+    <h2
+      className="flex flex-col lg:block text-center pt-12 text-5xl tracking-tight text-gray-900 md:text-6xl"
+      onClick={resume}
+    >
       <span className="mb-2 lg:mb-0">My superpower?</span>{' '}
       <span
         className={cn('text-primary-400 pt-4 pb-12 min-h-[10rem]', {
