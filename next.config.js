edf04240723f05bd6d0319afafdac854e760db3d
Johnny Magrippis <j@magrippis.com>
2022-03-04 22:10:50 +0100

    ⬆️  upgrade svgr to check video comment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+      use: [{loader: '@svgr/webpack', options: {icon: true}}],
     })
     return config
   },
